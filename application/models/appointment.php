ee0ac45afa1d3a8133d53ffe9c2ffc8116fd9d6b
Julio Foulquié <jfoulquie@gmail.com>
2014-08-15 00:30:11 +0200

    Start using date for get_events4today() function.

--- application/models/appointment.php
+++ application/models/appointment.php
@@ -8,7 +8,7 @@ class Appointment extends Eloquent {
 	public static $timestamps = true;
 
 	public static function get_events4today() {
-		return DB::table(self::$table)->where_between('a_date_ts', strtotime("today"), strtotime('tomorrow -1 second'))->get();
+		return DB::table(self::$table)->where_appointment_date(date('Y-m-d'))->get();
 	}
 
 
