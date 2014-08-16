ba697ce50477296500fd45812423ce67cd32ca8d
Julio Foulquié <jfoulquie@gmail.com>
2014-08-16 22:28:52 +0200

    Add relationships to appointment and places and new functions to the model.

--- application/models/appointment.php
+++ application/models/appointment.php
@@ -3,12 +3,17 @@
 
 class Appointment extends Eloquent {
 
+	public static $key  = 'id';
 	public static $table  = 'appointments';
 
 	public static $timestamps = true;
 
 	public static function get_events4today() {
 		return DB::table(self::$table)->where_appointment_date(date('Y-m-d'))->get();
+	}
+
+	public function place() {
+		return $this->belongs_to('Place');
 	}
 
 
--- application/models/place.php
+++ application/models/place.php
@@ -11,4 +11,21 @@ class Place extends Eloquent {
 		return $this->has_many('Place_Alias');
 	}
 
+	public function appointments() {
+		return $this->has_many('Appointment');
+	}
+
+	public function count_appointments() {
+		if(isset($this->id)) {
+			$cache_key = 'count_app:' . $this->id;
+// 			$cache = Cache::get($cache_key);
+// 			if(empty($cache)) {
+				$count = DB::table(Appointment::$table)->where_place_id($this->id)->count();
+// 				Cache::put($cache_key, $count, 1440); // Store for one day
+				return $count;
+// 			}
+		}
+		return 'Empty object!';
+	}
+
 }
