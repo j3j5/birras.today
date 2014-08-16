725c3195b18fb08e0fa9d247f52eeae85e30a1f3
Julio Foulquié <jfoulquie@gmail.com>
2014-08-16 22:26:44 +0200

    Add CSS for 404.

--- application/routes.php
+++ application/routes.php
@@ -63,6 +63,7 @@ Event::listen('404', function()
 {
 	Asset::container('header')->add('bootstrap_css', "/maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css");
 	Asset::container('header')->add('global', "css/global.css");
+	Asset::container('header')->add('404', "css/404.css");
 
 	Asset::container('footer')->add('jquery', '/code.jquery.com/jquery-1.11.1.min.js');
 	Asset::container('footer')->add('bootstrap_js', "//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js");
