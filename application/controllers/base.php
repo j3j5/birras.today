19a4c44aedec62b5cb77d2ccd9fa508f3983594e
Julio Foulquié <jfoulquie@gmail.com>
2014-08-17 18:10:01 +0200

    Add properly open sans font.

--- application/controllers/base.php
+++ application/controllers/base.php
@@ -8,7 +8,7 @@ class Base_Controller extends Controller {
 	 */
 	public function __construct() {
 		Asset::container('header')->add('bootstrap_css', "/maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css");
-		Asset::container('header')->add('open_sans', "/fonts.googleapis.com/css?family=Open+Sans:400,800,700,600,600italic,400italic");
+		Asset::container('header')->style('open_sans', "/fonts.googleapis.com/css?family=Open+Sans:400,800,700,600,600italic,400italic");
 		Asset::container('header')->add('global', "css/global.css");
 
 		Asset::container('footer')->add('jquery', '/code.jquery.com/jquery-1.11.1.min.js');
