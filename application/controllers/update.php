62e85b9419186e6342d6c62baa593125b370408b
Julio Foulquié <jfoulquie@gmail.com>
2014-08-19 01:40:00 +0200

    Add auth filter before update controller.

--- application/controllers/update.php
+++ application/controllers/update.php
@@ -4,6 +4,11 @@ class Update_Controller extends Base_Controller {
 
 	public $restful = true;
 
+	public function __construct() {
+		parent::__construct();
+		$this->filter('before', 'auth');
+	}
+
 	/**
 	 * Registration Page
 	 */
