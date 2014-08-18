83887284a56e498ae62fc7bc6877aee70e7833b0
Julio Foulquié <jfoulquie@gmail.com>
2014-08-18 11:22:47 +0200

    Tweak the login and register forms.

--- application/views/auth/login.blade.php
+++ application/views/auth/login.blade.php
@@ -3,25 +3,38 @@
 @section('content')
 	<h1>Login</h1>
 
-	{{ Form::open('auth/login', 'POST') }}
+	{{ Form::open('auth/login', 'POST', array('class' => 'form-horizontal')) }}
+		<fieldset>
+			<!-- username field -->
+			<div class="form-group">
+				{{ Form::label('username', 'Username', array('class' => 'control-label col-xs-2', 'for' => 'username')) }}
+				<div class="col-xs-10">
+					<div class="input-group">
+						{{ Form::text('username', Input::old('username')) }}
+					</div>
+				</div>
+			</div>
+			<!-- password field -->
+			<div class="form-group">
+				{{ Form::label('password', 'Password', array('class' => 'control-label col-xs-2', 'for' => 'password')) }}
+				<div class="col-xs-10">
+					<div class="input-group">
+						{{ Form::password('password') }}
+					</div>
+				</div>
+			</div>
 
-	<!-- username field -->
-	<p>
-		{{ Form::label('username', 'Username') }}<br/>
-		{{ Form::text('username', Input::old('username')) }}
-	</p>
+			<!-- csrf token	 -->
+			{{ Form::token() }}
 
-	<!-- password field -->
-	<p>
-		{{ Form::label('password', 'Password') }}<br/>
-		{{ Form::password('password') }}
-	</p>
-
-	<!-- csrf token	 -->
-	{{ Form::token() }}
-
-	<!-- submit button -->
-	<p>{{ Form::submit('Login', array('class' => 'btn my-btn')) }}</p>
-
+			<!-- submit button -->
+			<div class="form-group">
+				<div class="col-xs-offset-2 col-xs-10">
+					<div class="input-group">
+						{{ Form::submit('Login', array('class '=> "btn my-btn")) }}
+					</div>
+				</div>
+			</div>
+		</fieldset>
 	{{ Form::close() }}
 @endsection
--- application/views/auth/register.blade.php
+++ application/views/auth/register.blade.php
@@ -3,44 +3,72 @@
 @section('content')
 <h1>Sign up</h1>
 
-{{ Form::open('register', 'POST') }}
+{{ Form::open('register', 'POST', array('class' => 'form-horizontal')) }}
 
-<!-- username field -->
-<p>
-	{{ Form::label('username', 'Username') }}<br/>
-	{{ Form::text('username', Input::old('username')) }}
-</p>
+	<fieldset>
 
-<!-- password field -->
-<p>
-	{{ Form::label('password', 'Password') }}<br/>
-	{{ Form::password('password') }}
-</p>
+		<!-- username field -->
+		<div class="form-group">
+			{{ Form::label('username', 'Username', array('class' => 'control-label col-xs-2', 'for' => 'username')) }}
+			<div class="col-xs-10">
+				<div class="input-group">
+					{{ Form::text('username', Input::old('username')) }}
+				</div>
+			</div>
+		</div>
 
-<!-- repeat password field -->
-<p>
-	{{ Form::label('password2', 'Repeat Password') }}<br/>
-	{{ Form::password('password2') }}
-</p>
+		<!-- email -->
+		<div class="form-group">
+			{{ Form::label('email', 'E-mail', array('class' => 'control-label col-xs-2', 'for' => 'email')) }}
+			<div class="col-xs-10">
+				<div class="input-group">
+					{{ Form::text('email') }}
+				</div>
+			</div>
+		</div>
 
-<!-- Name -->
-<p>
-	{{ Form::label('name', 'Name') }}<br/>
-	{{ Form::text('name') }}
-</p>
+		<!-- password field -->
+		<div class="form-group">
+			{{ Form::label('password', 'Password', array('class' => 'control-label col-xs-2', 'for' => 'password')) }}
+			<div class="col-xs-10">
+				<div class="input-group">
+					{{ Form::password('password') }}
+				</div>
+			</div>
+		</div>
 
-<!-- email -->
-<p>
-	{{ Form::label('email', 'E-mail') }}<br/>
-	{{ Form::text('email') }}
-</p>
+		<!-- repeat password field -->
+		<div class="form-group">
+			{{ Form::label('password2', 'Repeat Password', array('class' => 'control-label col-xs-2', 'for' => 'password2')) }}
+			<div class="col-xs-10">
+				<div class="input-group">
+					{{ Form::password('password2') }}
+				</div>
+			</div>
+		</div>
 
+		<!-- Name -->
+		<div class="form-group">
+			{{ Form::label('name', 'Name', array('class' => 'control-label col-xs-2', 'for' => 'name')) }}
+			<div class="col-xs-10">
+				<div class="input-group">
+					{{ Form::text('name') }}
+				</div>
+			</div>
+		</div>
 
-<!-- csrf token	 -->
-{{ Form::token() }}
+		<!-- csrf token	 -->
+		{{ Form::token() }}
 
-<!-- submit button -->
-<p>{{ Form::submit('Register', array('class' => 'btn my-btn')) }}</p>
+		<!-- submit button -->
+		<div class="form-group">
+			<div class="col-xs-offset-2 col-xs-10">
+				<div class="input-group">
+					{{ Form::submit('Register', array('class '=> "btn my-btn")) }}
+				</div>
+			</div>
+		</div>
 
+	</fieldset>
 {{ Form::close() }}
 @endsection
